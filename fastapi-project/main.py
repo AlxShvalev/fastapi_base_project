e32ed250c48a7283ece22fc0bee29ee3b831be33
AlxShvalev <shvalevalx@gmail.com>
2026-03-23 21:52:43 +0500

    startapp actions

--- fastapi-project/main.py
+++ fastapi-project/main.py
@@ -1,11 +1,26 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 import uvicorn
 from fastapi import FastAPI
 
 from api import router as api_router
 from core.config import settings
+from core.models import db_helper
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    # startapp
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+
+app = FastAPI(
+    lifespan=lifespan,
+)
+
 app.include_router(
     api_router,
     prefix=settings.api.prefix,
