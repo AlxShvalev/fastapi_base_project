08e8c82f0387c821b41b9d6348aa73709f3f7b65
AlxShvalev <shvalevalx@gmail.com>
2026-03-28 20:17:32 +0500

    Add UserIdType

--- fastapi-project/core/database/models/user.py
+++ fastapi-project/core/database/models/user.py
@@ -6,6 +6,7 @@ from fastapi_users.db import (
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
+from core.types.user_id_type import UserIdType
 from .base import BaseModel
 from .mixins.pks import IntIdMixin
 from .mixins.timestamps import UpdatedAtMixin
@@ -14,10 +15,10 @@ if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(BaseModel, IntIdMixin, UpdatedAtMixin, SQLAlchemyBaseUserTable[int]):
+class User(BaseModel, IntIdMixin, UpdatedAtMixin, SQLAlchemyBaseUserTable[UserIdType]):
     first_name: Mapped[str] = mapped_column(String(64), nullable=True)
     last_name: Mapped[str] = mapped_column(String(64), nullable=True)
 
     @classmethod
     async def get_db(cls, session: "AsyncSession"):
-        yield SQLAlchemyUserDatabase(session, User)
+        yield SQLAlchemyUserDatabase(session, cls)
