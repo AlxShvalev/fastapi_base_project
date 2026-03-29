d9154faaa592150ab410f375518345be33bd140b
AlxShvalev <shvalevalx@gmail.com>
2026-03-29 19:21:08 +0500

    Refactored auth dependencies

--- fastapi-project/api/v1/auth.py
+++ fastapi-project/api/v1/auth.py
@@ -1,29 +1,17 @@
-from fastapi_users import FastAPIUsers
-
 from fastapi import APIRouter
-from api.dependencies.backend import authentication_backend
-from api.dependencies.user_manager import get_user_manager
+from api.dependencies.authentication.backend import authentication_backend
 from api.schemas.user import UserRead, UserCreate
-from core.database.models import User
-from core.types.user_id_type import UserIdType
+from api.dependencies.authentication.fastapi_users_dependencies import fastapi_users
 
 
-fastapi_users = FastAPIUsers[User, UserIdType](
-    get_user_manager,
-    [authentication_backend],
-)
-
-
-router = APIRouter(prefix="/auth", tags=["auth"])
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
 router.include_router(
     fastapi_users.get_auth_router(authentication_backend),
     prefix="/jwt",
-    tags=["auth"],
 )
 
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate),
     prefix="",
-    tags=["auth"],
 )
