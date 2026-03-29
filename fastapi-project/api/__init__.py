f2cd1946c484696f682b077488ecc839bd22908e
AlxShvalev <shvalevalx@gmail.com>
2026-03-29 19:23:02 +0500

    Refactored users schemas

--- fastapi-project/api/__init__.py
+++ fastapi-project/api/__init__.py
@@ -4,4 +4,4 @@ from .v1.auth import router as auth_router
 
 router = APIRouter()
 
-router.include_router(auth_router, tags=["auth"])
+router.include_router(auth_router)
--- fastapi-project/api/schemas/user.py
+++ fastapi-project/api/schemas/user.py
@@ -1,17 +1,21 @@
 from fastapi_users import schemas
+from pydantic import BaseModel
 
 from core.types.user_id_type import UserIdType
 
 
-class UserCreate(schemas.BaseUserCreate):
+class UserExtraFields(BaseModel):
     first_name: str | None
     last_name: str | None
 
 
-class UserRead(schemas.BaseUser[UserIdType]):
-    first_name: str | None
-    last_name: str | None
+class UserCreate(UserExtraFields, schemas.BaseUserCreate):
+    pass
 
 
-class UserUpdate(UserCreate, schemas.BaseUserUpdate):
+class UserRead(UserExtraFields, schemas.BaseUser[UserIdType]):
+    pass
+
+
+class UserUpdate(UserExtraFields, schemas.BaseUserUpdate):
     pass
