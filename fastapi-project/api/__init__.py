35ade6e0a4877bcbace4ccf461d5ee5ead0d91a7
AlxShvalev <shvalevalx@gmail.com>
2026-03-29 19:52:58 +0500

    refactored v1 router

--- fastapi-project/api/__init__.py
+++ fastapi-project/api/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
-from .v1.auth import router as auth_router
+from .v1 import router as v1_router
 
 
 router = APIRouter()
 
-router.include_router(auth_router)
+router.include_router(v1_router)
